7b9d6297691b08e7d3c56e44ddea3e3440c008e7
hammadsadi <hammad.sadi@yahoo.com>
2025-12-03 10:32:13 +0600

    Refactor Config and DB connection

---
+++ src/config/db.ts
@@ -0,0 +1,33 @@
+import { Pool } from "pg";
+import config from ".";
+// Database
+export const pool = new Pool({
+  connectionString: config.CONNECTION_STRING,
+});
+
+export const initDb = async () => {
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255),
+      email VARCHAR(255) UNIQUE NOT NULL,
+      age INT,
+      address TEXT,
+      createdAT TIMESTAMP DEFAULT NOW(),
+      updatedAT TIMESTAMP DEFAULT NOW()
+      )
+    `);
+
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS TODOS(
+      id SERIAL PRIMARY KEY,
+      userId INT REFERENCES users(id),
+      title VARCHAR(255),
+      description TEXT,
+      dueDate DATE, 
+      completed BOOLEAN,
+      createdAT TIMESTAMP DEFAULT NOW(),
+      updatedAT TIMESTAMP DEFAULT NOW()
+
+      )`);
+};
--- src/server.ts
+++ src/server.ts
@@ -1,57 +1,116 @@
-import express, { Request, Response } from "express";
-import { Pool } from "pg";
-import path from "path";
-import dotenv from "dotenv";
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+import express, { NextFunction, Request, Response } from "express";
+
+import config from "./config";
+import { initDb, pool } from "./config/db";
+
 const app = express();
-
-// Database
-const pool = new Pool({
-  connectionString: process.env.CONNECTION_STRING,
-});
-
-const initDb = async () => {
-  await pool.query(`
-      CREATE TABLE IF NOT EXISTS users(
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(255),
-      email VARCHAR(255) UNIQUE NOT NULL,
-      age INT,
-      address TEXT,
-      createdAT TIMESTAMP DEFAULT NOW(),
-      updatedAT TIMESTAMP DEFAULT NOW()
-      )
-    `);
-
-  await pool.query(`
-      CREATE TABLE IF NOT EXISTS TODOS(
-      id SERIAL PRIMARY KEY,
-      userId INT REFERENCES users(id),
-      title VARCHAR(255),
-      description TEXT,
-      dueDate DATE, 
-      completed BOOLEAN,
-      createdAT TIMESTAMP DEFAULT NOW(),
-      updatedAT TIMESTAMP DEFAULT NOW()
-
-      )`);
-};
 
 initDb();
 
+//  Logger
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}`);
+  next();
+};
+
 //  Parser
 app.use(express.json());
-app.get("/", (req: Request, res: Response) => {
+app.get("/", logger, (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.post("/create", (req: Request, res: Response) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: "Created",
+//  Users Routes
+
+app.post("/create-user", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      `
+      INSERT INTO users(name, email) VALUES ($1, $2) RETURNING *
+      `,
+      [name, email]
+    );
+    res.status(201).json({
+      success: true,
+      message: "Created user successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users`);
+    res.status(200).json({
+      success: true,
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [
+      req.params.id,
+    ]);
+    res.status(result.rows.length > 0 ? 200 : 404).json({
+      success: result.rows.length > 0 ? true : false,
+      message:
+        result.rows.length > 0
+          ? "User Retrieved Successfully"
+          : "User not found",
+      data: result.rows?.length > 0 ? result.rows[0] : [],
+    });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.patch("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      `UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *`,
+      [req.body.name, req.body.email, req.params.id]
+    );
+    res.status(result.rows.length > 0 ? 200 : 404).json({
+      success: result.rows.length > 0 ? true : false,
+      message:
+        result.rows.length > 0 ? "User Updated Successfully" : "User not found",
+      data: result.rows?.length > 0 ? result.rows[0] : [],
+    });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`DELETE FROM users WHERE id = $1`, [
+      req.params.id,
+    ]);
+    res.status(result?.rowCount || 0 > 0 ? 200 : 404).json({
+      success: result.rows.length > 0 ? true : false,
+      message:
+        result.rows.length > 0 ? "User Deleted Successfully" : "User not found",
+      data: result.rows?.length > 0 ? result.rows[0] : [],
+    });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Api Not Found
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "API not found",
   });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Example app listening on port ${config.PORT}`);
 });
